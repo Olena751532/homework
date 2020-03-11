f904ab83d28d821664d0e2f72743fa5ea06e2c48
Olena Podolian <alyona751532@gmail.com>
2020-03-12 00:16:42 +0200

    hw1-2

---
+++ hw_1-2.py
@@ -0,0 +1,15 @@
+#homework 1_1
+a = int(input(" your number a : "))
+b = int(input(" your number b : "))
+
+print ("a + b = ", a + b)
+print ("a - b = ", a - b)
+print ("a / b = ", a / b)
+print ("a * b = ", a * b)
+
+
+# homework 1.2
+name = input("What is your name?: ")
+age = int(input("How old are you?: "))
+adress = input("Where do you live?: ")
+print ("Hello " + name + " You are " + str(age) + " You live in " + adress)
